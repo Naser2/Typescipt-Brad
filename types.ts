8ac98211ffd27398ce4e7e6e272fe46f19ffa592
Nas <nassersanou23@gmail.com>
2019-06-18 15:26:21 -0400

    Types commit

---
+++ types.ts
@@ -0,0 +1,20 @@
+console.log('hello world from ts');
+
+var myString: string;
+let myNumb: number;
+let myVar: any;
+let strArr: Array<string>;
+let numbArr: number[];
+let myBool: boolean;
+let myTuple: [string, number, number, number, string];
+let myStrNumbTuple: [string, number];
+let myVoid: void = undefined;
+myString = 'Hello World' + ' I work'.slice(0, 6);
+myNumb = 3;
+myBool = false;
+myVar = true;
+strArr = ['khlhi', 'worls', 'errrr'];
+numbArr = [2, 4, 5];
+myTuple = ['Tuple', 2, 4, 5, 'Hello'];
+myStrNumbTuple = ['Tuple', 2];
+console.log(myVoid);
